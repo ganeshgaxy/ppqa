621f6454c91ae912754de3e1e39825e437f109cd
ganesh <gan@stylelabs.com>
2022-06-12 16:46:54 +0200

    removed tauri for now

--- packages/qa-framework/tauri-gui/components/test/view/TestView.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Layer } from 'react-konva';
-
-export const TestView = () => {
-  return <Layer></Layer>;
-};
--- packages/qa-framework/tauri-gui/pages/explore.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next';
-import React from 'react';
-
-const Explore: NextPage = () => {
-  return <div>explore</div>;
-};
-
-export default Explore;
