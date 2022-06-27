98efe274e8d218bf1875767969a9485bc75b602d
ganesh <gan@stylelabs.com>
2022-06-27 22:49:49 +0200

    Updated rust package usage

--- packages/qa-framework/performance/performance-rust/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#[no_mangle]
-pub fn add(first:i32, second:i32) ->i32 {
-    first+second
-}
--- packages/qa-framework/performance/src/index.ts
+++ packages/qa-framework/performance/src/index.ts
@@ -1,12 +1,36 @@
 import ffi from 'ffi-napi';
 import path from 'path';
 
-const libPath = path.resolve(
-  path.join(
-    __dirname.split('/dist')[0],
-    './performance-rust/target/release/libperformance_rust.dylib'
-  )
-);
+import os from 'os';
+
+// Printing os.type() value
+let libPath = '',
+  libForOS = '';
+var type = os.type();
+switch (type) {
+  case 'Darwin':
+    libForOS = './performance-rust/target/release/performance_rust.dylib';
+    libPath = path.resolve(
+      path.join(__dirname.split('/performance/dist')[0], libForOS)
+    );
+    break;
+  case 'Linux':
+    libForOS = './performance-rust/target/release/performance_rust.so';
+    libPath = path.resolve(
+      path.join(__dirname.split('/performance/dist')[0], libForOS)
+    );
+    break;
+  case 'Windows_NT':
+    libForOS = './performance-rust/target/release/performance_rust.dll';
+    libPath = path.resolve(
+      path.join(__dirname.split('\\performance\\dist')[0], libForOS)
+    );
+    break;
+  default:
+    libPath = path.resolve(
+      path.join(__dirname.split('\\performance\\dist')[0], libForOS)
+    );
+}
 
 const libWeb = ffi.Library(libPath, {
   add: ['int32', ['int32', 'int32']],
